e8ac1e79efbaef78d54fe95888f79730dcad6b74
tiagohsoares <tiagowenceslau11@gmail.com>
2025-06-30 11:41:27 -0300

    add: home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\ResumeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ResumeController::class , 'index']);

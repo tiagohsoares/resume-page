4e5eef52f0b370a76a3adb8af14832b3d9bd9a73
tiagohsoares <tiagowenceslau11@gmail.com>
2025-07-01 12:13:00 -0300

    Resume Controller

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -6,12 +6,14 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Storage;
+use App\DataObjects\Resume;
 
 class ResumeController extends Controller
 {
     public function index(Factory $view){
 
-        storage::get('resume.json');
-        return $view->make('resume');
+        $resumes = storage::disk('resumes')->get('resume.json');
+        $resumeData = json_decode($resumes, true);  
+        return $view->make('resume' , ['resume' => Resume::fromArray($resumeData)]);
     }
 }

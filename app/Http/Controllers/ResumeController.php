c030343e347bb7d5fde332ee2014ee8fbb0f3290
tiagohsoares <tiagowenceslau11@gmail.com>
2025-07-10 11:44:46 -0300

    add: Caching

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -7,13 +7,32 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Storage;
 use App\DataObjects\Resume;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Support\Facades\Cache;
 
 class ResumeController extends Controller
 {
     public function index(Factory $view){
+        $resume = Cache::remember('resumeData', now()->addDay(), function () {
+            $resumes = storage::disk('resumes')->get('resume.json');
+            $resumeData = json_decode($resumes, true);
 
-        $resumes = storage::disk('resumes')->get('resume.json');
-        $resumeData = json_decode($resumes, true);  
-        return $view->make('resume' , ['resume' => Resume::fromArray($resumeData)]);
+            return Resume::fromArray($resumeData);
+        });
+
+        return $view->make('resume' , ['resume' => $resume]);
+    }
+
+    public function download ()
+    {
+        $resume = Cache::remember('resumeData', now()->addDay(), function () {
+            $resumes = storage::disk('resumes')->get('resume.json');
+            $resumeData = json_decode($resumes, true);
+
+            return Resume::fromArray($resumeData);
+        });
+
+        $pdf = Pdf::loadView('resume', ['resume' => $resume]);
+        return $pdf->download($resume->basics->name . '-resume.pdf');
     }
 }
